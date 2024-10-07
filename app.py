611be3e89fc5d781584567455dda43ee1e510bbd
Juan Antonio Pineda Amador <juanantpiama@gmail.com>
2024-10-07 18:39:06 +0200

    Se ha añaido nuevas funcionalidades a la pagina web

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, abort, redirect,request
-import os, MySQLdb
-
+from flask import Flask, render_template, request
+import MySQLdb
 
 app = Flask(__name__)
 
@@ -8,6 +7,21 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
+@app.route('/datos', methods=["POST"])
+def datos():
+    usuario = request.form.get("usuario")
+    contrasenia = request.form.get("contrasenia")
+    try:
+        db = MySQLdb.connect("192.168.0.27", usuario, contrasenia, "prueba")
+        sql="SHOW TABLES"
+        cursor = db.cursor()
+        cursor.execute(sql)
+        tablas = cursor.fetchall()
+
+        return render_template('datos.html',tablas=tablas,conexion="bien")
+    except MySQLdb.Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return render_template('datos.html', conexion="mal")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
