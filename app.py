32bec571c2f948b0e7d4f229f66cd377999bb964
Juan Antonio Pineda Amador <juanantpiama@gmail.com>
2024-10-18 16:53:53 +0200

    Se ha modificado los cambios en la pagian web

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import MySQLdb
 
+db = None  # Definir db como global
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,8 +13,11 @@ def inicio():
 def datos():
     usuario = request.form.get("usuario")
     contrasenia = request.form.get("contrasenia")
+    basedatos = request.form.get("basedatos")
     try:
-        db = MySQLdb.connect("192.168.0.27", usuario, contrasenia, "prueba")
+        global db 
+        db = MySQLdb.connect("192.168.0.29", usuario, contrasenia, basedatos)
+
         sql="SHOW TABLES"
         cursor = db.cursor()
         cursor.execute(sql)
@@ -20,8 +25,21 @@ def datos():
 
         return render_template('datos.html',tablas=tablas,conexion="bien")
     except MySQLdb.Error as e:
-        print(f"Error al conectar a la base de datos: {e}")
         return render_template('datos.html', conexion="mal")
+
+@app.route('/tablas/<tabla>')
+def detalle(tabla):
+    try:
+        cursor = db.cursor()
+        cursor.execute(f"SHOW COLUMNS FROM {tabla}")
+        columnas = cursor.fetchall()
+
+        cursor.execute(f"SELECT * FROM {tabla}")
+        filas = cursor.fetchall()
+
+        return render_template('tablas.html', tabla=tabla, columnas=columnas, filas=filas)
+    except MySQLdb.Error as e:
+        return f"Error en la conexión: {str(e)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
